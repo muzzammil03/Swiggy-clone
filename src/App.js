01153f162bbeae75a0996a3482543f803c614395
Muzzammil Ahmed <muzzammilahmad62435@gmail.com>
2025-02-16 20:13:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import './App.css';
-import Category from './components/Category.jsx';
+// import Category from './components/Category.jsx';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
-import OnlineDelivery from './components/OnlineDelivery.jsx';
-import TopResturant from './components/TopResturant.jsx';
+// import OnlineDelivery from './components/OnlineDelivery.jsx';
+// import TopResturant from './components/TopResturant.jsx';
 import Search from './routes/Search.jsx';
+import SignIn from './routes/SignIn.jsx';
+import Help from './routes/Help.jsx';
+import { useState } from 'react';
+import  Home  from './routes/Home.jsx';
+import { Routes, Route } from "react-router-dom";
+import Offers from './routes/Offers.jsx';
+import Cart from './routes/Cart.jsx';
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
    <>
-   <Header/>
-   <Category/>
-   <TopResturant/>
-   <OnlineDelivery/>
-   <Footer/>
-
+   <Header setIsOpen={setIsOpen}/>
+   <SignIn isOpen={isOpen} setIsOpen={setIsOpen} />
    {/* <Search/> */}
+   {/* <Help/> */}
+   <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/offers" element={<Offers/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+       
+
+      </Routes>
    </>
   );
 }
-
+{/* <Route path="/offers" element={<About />} /> */}
 export default App;
